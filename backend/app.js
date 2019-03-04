3dc3f5b59d0eb7d33340222e953751c5e3f24360
teepobH <teepob.haru@gmail.com>
2019-03-04 10:07:49 +0700

    path to index

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ mongoose
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join("backend/images")));
+app.use("/images", express.static(path.join(__dirname, "backend/images")));
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -40,9 +40,9 @@ app.use("/api/posts", postsRoutes);
 app.use("/api/user", userRoutes);
 
 //DEPLOY
-app.use(express.static(__dirname + '/dist/meantest'));
+app.use(express.static(path.join(__dirname, '../dist/meantest')));
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/meantest/index.html'));
+    res.sendFile(path.join(__dirname, '../dist/meantest/index.html'));
 });
 
 module.exports = app;
